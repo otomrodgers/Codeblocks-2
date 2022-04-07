0438547b048edef94441320f4517ec74a19fde85
otomrodgers <you@example.com>
2022-04-07 16:05:08 -0700

    Finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of char: %ulu byte(s)\n", sizeof(a));
 	printf("Size of int: %ulu byte(s)\n", sizeof(b));
-	printf("Size of long: %ulu byte(s)\n", sizeof(a));
+	printf("Size of long: %ulu byte(s)\n", sizeof(c));
 	printf("Size of long long: %ulu byte(s)\n", sizeof(d));
 	printf("Size of float: %ulu byte(s)\n", sizeof(e));
 
