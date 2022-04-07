44e65cb71016d9388a4348b5135d67f5df173c52
otomrodgers <you@example.com>
2022-04-07 15:57:41 -0700

    I hope it works

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
 /**
- * This program prints sizes of 
- * different datat types
- * in C program
+ *init: project initialization
+ *Main: main function
+ *Description: This program prints sizes of
+ *Different datat types
  */
 
 int main(void)
-
 {
 	char a;
 	int b;
 	long c;
-        double d;
+	double d;
 	float e;
-
 
 	printf("Size of char: %ulu byte(s)\n", sizeof(a));
 	printf("Size of int: %ulu byte(s)\n", sizeof(b));
 	printf("Size of long: %ulu byte(s)\n", sizeof(a));
 	printf("Size of long long: %ulu byte(s)\n", sizeof(d));
 	printf("Size of float: %ulu byte(s)\n", sizeof(e));
-
 	return (0);
 }
 
