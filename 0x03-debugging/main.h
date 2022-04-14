01d309aaf95fa1642df22d9916cf8b8540706a15
otomrodgers <you@example.com>
2022-04-14 00:29:24 -0700

    New way of doing things

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,45 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - test function to print if integer is positive
+ * or negative numbers
+ * @i: integer
+ */
+
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+/**
+* largest_number - returns the largest of 3 number
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+int largest_number(int a, int b, int c);
+
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap year into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year);
+
+/**
+ * convert_day - convert day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day);
+
+#endif /* _MAIN_H */
