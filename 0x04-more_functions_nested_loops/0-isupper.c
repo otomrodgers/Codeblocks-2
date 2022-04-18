cb4b30a76ec6c4791e8c3c1b9e33d5c83a7a18d0
otomrodgers <you@example.com>
2022-04-18 04:21:18 -0700

    Is uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <_putchar.c>
+
+/**
+ * Checks for upper case character
+ * _isupper returns 1 if true
+ * c is the character variable
+ */
+
+int _isupper(int c);
+
+int main(void)
+{
+	char c;
+
+	c = 'A'
+
+	if (_isupper(c))
+		return (1);
+	return (0);
+}
