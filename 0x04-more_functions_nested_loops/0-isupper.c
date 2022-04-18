38cfad3984319849cf5555caed9f89c83b385e3e
otomrodgers <you@example.com>
2022-04-18 04:25:37 -0700

    Uppercase letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,16 @@
-#include <main.h>
-#include <_putchar.c>
+#include "main.h"
 
 /**
- * Checks for upper case character
- * _isupper returns 1 if true
- * c is the character variable
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
 
-int _isupper(int c);
-
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A'
-
-	if (_isupper(c))
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	return (0);
 }
