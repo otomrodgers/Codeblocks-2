e96e4d2ebf67ebb25f641a2da50e224a4b21e7ba
otomrodgers <you@example.com>
2022-04-18 06:17:56 -0700

    Fizz Buzz with While loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,32 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (i <= 100)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		} else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		} else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		} else
-		{
-			printf("%d ", i);
+			printf("FizzBuzz");
 		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
 	putchar('\n');
 	return (0);
