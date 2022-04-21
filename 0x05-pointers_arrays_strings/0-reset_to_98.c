f87503e72228a3d62d9190e01353b73cdbecdcb1
otomrodgers <you@example.com>
2022-04-21 06:34:55 -0700

    Reset pointer function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - updates the value of int
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
